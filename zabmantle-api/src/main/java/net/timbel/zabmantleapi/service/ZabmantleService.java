6567298f68c6bd8613a0a5456d067e6e96ceef91
greenn-lab <greenn.lab@gmail.com>
2023-02-20 22:21:37 +0900

    avoid blocking

--- zabmantle-api/src/main/java/net/timbel/zabmantleapi/service/ZabmantleService.java
+++ zabmantle-api/src/main/java/net/timbel/zabmantleapi/service/ZabmantleService.java
@@ -1,22 +1,28 @@
 package net.timbel.zabmantleapi.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.timbel.zabmantleapi.dto.ZabmantleResponseDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
+@Slf4j
 public class ZabmantleService {
 
     private final WebClient client = WebClient
         .create("https://semantle-ko.newsjel.ly/guess/")
         .mutate().build();
 
-    public ZabmantleResponseDTO guess(String word) {
+    public Mono<ZabmantleResponseDTO> guess(String word) {
         return client
             .get()
-            .uri("{puzzle}/{word}", 307, word)
-            .retrieve()
-            .bodyToMono(ZabmantleResponseDTO.class)
-            .block();
+            .uri("{puzzle}/{word}", 325, word)
+            .exchangeToMono(response ->
+                response.bodyToMono(ZabmantleResponseDTO.class)
+            )
+            .log()
+            ;
+
     }
 }
